1883f2b30b72146d8b380036f776d16b8eccbbae
EdwardGoomba <edward@dock90.io>
2019-11-12 00:00:02 -0500

    Start typography setup

---
+++ src/components/Typography.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1``;
+const H2 = styled.h2``;
+const H3 = styled.h3``;
+const H4 = styled.h4``;
+const H5 = styled.h5``;
+const H6 = styled.h6;
+
+const BodyText = styled.p``;
+const AltText = styled.p``;
+const LinkText = styled.href``;
+
+export { H1, H2, H3, H4, H5, H6, BodyText, AltText, LinkText };

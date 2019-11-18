440bf68dbf67566f9d0bda05d18e090e44485278
EdwardGoomba <edward@dock90.io>
2019-11-18 18:04:12 -0500

    Adjust branding

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -13,19 +13,33 @@ import SignupConfirm from '../views/Auth/SignupConfirm';
 import { H1, H2 } from '../components/Typography';
 
 // styles
-const Branding = styled.div`
+const Layout = styled.div`
+  display: flex;
+  align-items: flex-end;
   height: 100vh;
   background: #bbbbbb;
+  background: url('https://bitbucket.org/equipter/crm_fe/raw/1304d057ec04ee6cfa940132c45b7a5ce52f39d0/src/img/bg.jpg');
 `;
+
+const Branding = styled.div`
+  color: #ffffff;
+  margin-left: 3rem;
+  margin-bottom: 5rem;
+  max-width: 300px;
+`;
+
+// TODO: add responsive styles
 
 const AuthLayout = () => (
   <Router>
     <Grid container>
       <Grid item xs={6}>
-        <Branding>
-          <H1>CRM Beta</H1>
-          <H2>Customer Relationship Management</H2>
-        </Branding>
+        <Layout>
+          <Branding>
+            <H1>CRM Beta</H1>
+            <H2>Customer Relationship Management</H2>
+          </Branding>
+        </Layout>
       </Grid>
       <Grid item xs={6}>
         <Route exact path="/" render={() => <Redirect to="/auth/login" />} />

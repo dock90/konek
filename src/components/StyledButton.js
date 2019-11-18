2870eecb9dea233a1cdaf60c83c295cb47f1fe53
EdwardGoomba <edward@dock90.io>
2019-11-18 18:41:20 -0500

    Layout signup page, adjust styles

---
+++ src/components/StyledButton.js
@@ -0,0 +1,11 @@
+import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const StyledButton = styled(Button)({
+  height: 40,
+  margin: 0,
+  padding: 0,
+  marginBottom: 30,
+});
+
+export { StyledButton };

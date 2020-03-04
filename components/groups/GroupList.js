2170087004fe435707b2f86f4b1c0500b0c5376e
Jeffrey Brubaker <jeffreymb@gmail.com>
2020-03-04 12:31:25 -0500

    Groups: Add key to items in GroupList.

--- components/groups/GroupList.js
+++ components/groups/GroupList.js
@@ -2,13 +2,7 @@ import { useQuery } from "@apollo/react-hooks";
 import { Grid } from "@material-ui/core";
 import { GROUPS_QUERY } from "../../queries/GroupQueries";
 import Loading from "../Loading";
-import styled from "styled-components";
 import GroupItem from "./GroupItem";
-
-const ListContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
 
 const GroupList = () => {
   const { loading, data, error } = useQuery(GROUPS_QUERY);
@@ -23,12 +17,8 @@ const GroupList = () => {
   return (
     <Grid container spacing={2}>
       {data.groups.map(group => (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <GroupItem
-            key={group.groupId}
-            group={group}
-            groupList={data.groups}
-          />
+        <Grid key={group.groupId} item xs={12} sm={6} md={4} lg={3} xl={2}>
+          <GroupItem group={group} groupList={data.groups} />
         </Grid>
       ))}
     </Grid>
